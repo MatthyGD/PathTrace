697a6d40b400ecac0c2b4aa519a0be9241e1e6dc
MatthyGD <130808445+MatthyGD@users.noreply.github.com>
2024-12-21 04:07:28 +0100

    Update PathTrace.sh

--- PathTrace.sh
+++ PathTrace.sh
@@ -7,21 +7,21 @@ clear
 
 # Optimizando la terminal
 echo -e "\033[1;32m========================= 🌱 Optimizando Terminal 🌱 =========================\033[0m"
-sleep 1
+sleep 0.5
 echo -e "\033[1;33m1: Ajustando la variable TERM para una mejor compatibilidad.\033[0m"
-sleep 1
+sleep 0.5
 export TERM=xterm
 echo -e "\033[1;33m2: Configurando el shell predeterminado a Bash.\033[0m"
-sleep 1
+sleep 0.5
 export SHELL=bash
 echo -e "\033[1;33m3: Activando soporte de colores en la terminal.\033[0m"
-sleep 1
+sleep 0.5
 export TERM=xterm-256color
 echo -e "\033[1;33m4: Aplicando cambios a la configuración del Bash.\033[0m"
-sleep 1
+sleep 0.5
 source /etc/skel/.bashrc
 echo -e "\033[1;33m5: Ajustando el tamaño de las columnas y filas para mejor visualización.\033[0m"
-sleep 1
+sleep 0.5
 stty rows 61 columns 229
 
 echo -e "\n"
@@ -29,86 +29,89 @@ sleep 1
 
 # Reconocimiento del Sistema
 echo -e "\033[1;32m========================= 🖥️ Reconocimiento del Sistema 🖥️ =========================\033[0m"
-sleep 1
-lsb_release -a
+sleep 0.5
+echo -e "\033[1;33m1: Contemplando la distribución del sistema operativo.\033[0m"
+sleep 0.5
+lsb_release -d | cut -f2- 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
-# Reconocimiento de la Version de la Bash
-echo -e "\033[1;32m========================= 🐚 Reconocimiento de la Version de la Bash 🐚 =========================\033[0m"
-sleep 1
-/bin/bash --version | grep "release"
+echo -e "\033[1;33m2: Contemplando la versión del kernel.\033[0m"
+sleep 0.5
+uname -r 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
-# Reconocimiento de la Version del Sudo
-echo -e "\033[1;32m========================= 🔑 Reconocimiento de la Version del Sudo 🔑 =========================\033[0m"
-sleep 1
-sudo -V | grep "version"
+echo -e "\033[1;33m3: Contemplando la versión de la Bash.\033[0m"
+sleep 0.5
+/bin/bash --version | grep -oP '\d+\.\d+\.\d+' 2>/dev/null
+
+echo -e "\n"
+
+echo -e "\033[1;33m4: Contemplando la versión del Sudo.\033[0m"
+sleep 0.5
+sudo -V | grep -i "sudo" | head -n 1 | awk '{print $3}' 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando archivos sensibles
 echo -e "\033[1;31m========================= 🚨 Listando Archivos Sensibles 🚨 =========================\033[0m"
-sleep 1
-ls -la /etc/passwd
-ls -la /etc/shadow
-ls -la /etc/sudoers
-ls -la /usr/share/ssl-cert/
-ls -la /proc/sched_debug/
-find / -name ".bashrc" -ls 2>/dev/null
-find / -name ".bash_history" -ls 2>/dev/null
-find / -name ".mysql_history" -ls 2>/dev/null
-find / -name "user.txt" -ls 2>/dev/null
-find / -name "root.txt" -ls 2>/dev/null
+sleep 0.5
+ls -l /etc/passwd 2>/dev/null
+ls -l /etc/shadow 2>/dev/null
+ls -l /etc/group 2>/dev/null
+ls -l /etc/sudoers 2>/dev/null
+ls -l /usr/share/ssl-cert/ 2>/dev/null
+ls -l /proc/sched_debug/ 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando el directorio home y usuarios
 echo -e "\033[1;32m========================= 🏠 Listando el Directorio Home y Usuarios 🏠 =========================\033[0m"
+sleep 0.5
 echo -e "\033[1;33m1: Usuarios Válidos del sistema.\033[0m"
-sleep 1
-cat /etc/passwd | grep "bash"
-cat /etc/passwd | grep "zsh"
+sleep 0.5
+cat /etc/passwd | grep "bash" 2>/dev/null
+cat /etc/passwd | grep "zsh" 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m2: Directorio Home.\033[0m"
-sleep 1
-ls -la /home/
+sleep 0.5
+ls -la /home/ 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando historial del usuario
 echo -e "\033[1;32m========================= 📝 Listando Historial & Variables del Usuario 📝 =========================\033[0m"
-sleep 1
-history
-env
-alias
+sleep 0.5
+history 2>/dev/null
+env 2>/dev/null
+alias 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando información del usuario
 echo -e "\033[1;34m========================= 👤 Listando Información del Usuario 👤 =========================\033[0m"
-sleep 1
-id
+sleep 0.5
+id 2>/dev/null
+
 echo -e "\n"
+
 echo -e "\033[1;34m========================= 📜 Apreta Ctrl + C en caso de no conocer las credenciales 📜 =========================\033[0m"
-sudo -l
+sleep 0.5
+sudo -l 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando Llaves SSH
 echo -e "\033[1;31m========================= 🔐 Listando Llaves SSH 🔐 =========================\033[0m"
-sleep 1
+sleep 0.5
 find / -name "id_rsa" 2>/dev/null
 find / -name "id_rsa.pub" 2>/dev/null
 find / -name "authorized_keys" 2>/dev/null
@@ -116,80 +119,106 @@ find / -name "authorized_keys" 2>/dev/null
 echo -e "\n"
 sleep 1
 
-# Listando Posibles archivos Backup
-echo -e "\033[1;31m========================= 💾 Listando Posibles Archivos Backup 💾 =========================\033[0m"
+# Listando Posibles archivos Comprimidos
+echo -e "\033[1;31m========================= 📚 Listando Archivos Comprimidos 📚 =========================\033[0m"
+sleep 0.5
+echo -e "\033[1;33m1: Listando Archivos con Extension Zip.\033[0m"
+sleep 0.5
+find / -name "*.zip" 2>/dev/null
+
+echo -e "\n"
+
+echo -e "\033[1;33m2: Listando Archivos con Extension Rar.\033[0m"
+sleep 0.5
+find / -name "*.rar" 2>/dev/null
+
+echo -e "\n"
+
+echo -e "\033[1;33m3: Listando Archivos que contemplen el nombre 7z.\033[0m"
+sleep 0.5
+find / -name "*.7z" 2>/dev/null
+
+echo -e "\n"
+
+echo -e "\033[1;33m4: Listando Archivos que contemplen el nombre Tar.\033[0m"
+sleep 0.5
+find / -name "*.tar" 2>/dev/null
+
+echo -e "\n"
 sleep 1
+
+# Listando Posibles archivos Backup
+echo -e "\033[1;31m========================= 💾 Listando Archivos Backup 💾 =========================\033[0m"
+sleep 0.5
 echo -e "\033[1;33m1: Listando Archivos con Extension Bak.\033[0m"
+sleep 0.5
 find / -name "*.bak" 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m2: Listando Archivos con Extension Old.\033[0m"
-sleep 1
+sleep 0.5
 find / -name "*.old" 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m3: Listando Archivos que contemplen el nombre Backup.\033[0m"
-sleep 1
+sleep 0.5
 find / -name "*backup*" 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando Posibles archivos de Bases de Datos
-echo -e "\033[1;31m========================= 🗄️ Listando Posibles Archivos de Bases de Datos 🗄️ =========================\033[0m"
-sleep 1
+echo -e "\033[1;31m========================= 🗄️ Listando Archivos de Bases de Datos 🗄️ =========================\033[0m"
+sleep 0.5
 echo -e "\033[1;33m1: Listando Archivos de Configuración de Bases de Datos.\033[0m"
+sleep 0.5
 find / -name "*config.php" 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m2: Listando Archivos de Bases de Datos MYSQL.\033[0m"
-sleep 1
+sleep 0.5
 find / -name "*.sql" 2>/dev/null
 find / -name "*.db" 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m3: Listando Archivos de Bases de Datos SQLite3.\033[0m"
-sleep 1
+sleep 0.5
 find / -name "*.sqlite3" 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando Posibles Gestores de Contraseñas
-echo -e "\033[1;31m========================= 🔐 Listando Posibles Gestores de Contraseñas 🔐 =========================\033[0m"
-sleep 1
+echo -e "\033[1;31m========================= 🔐 Listando Gestores de Contraseñas 🔐 =========================\033[0m"
+sleep 0.5
 echo -e "\033[1;33m1: Listando archivos de KeePass.\033[0m"
+sleep 0.5
 find / -name "*.kdbx" 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m2: Listando archivos de Password Safe.\033[0m"
-sleep 1
+sleep 0.5
 find / -name "*.psafe3" 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando Posibles Archivos Sensibles de FireFox
-echo -e "\033[1;31m========================= 🦊 Listando Posibles Archivos Sensibles de FireFox 🦊 =========================\033[0m"
-sleep 1
+echo -e "\033[1;31m========================= 🦊 Listando Archivos Sensibles de FireFox 🦊 =========================\033[0m"
+sleep 0.5
 echo -e "\033[1;33m1: Listando archivos Key4.db de Firefox.\033[0m"
+sleep 0.5
 find / -name "key4.db" 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m2: Listando archivos Logins.json de Firefox.\033[0m"
-sleep 1
+sleep 0.5
 find / -name "logins.json" 2>/dev/null
 
 echo -e "\n"
@@ -197,15 +226,15 @@ sleep 1
 
 # Listando binarios SUID
 echo -e "\033[1;33m========================= 🔧 Listando Binarios SUID 🔧 =========================\033[0m"
-sleep 1
+sleep 0.5
 echo -e "\033[1;33m1: SUID\033[0m"
+sleep 0.5
 find / -perm -4000 -ls 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m2: GUID\033[0m"
-sleep 1
+sleep 0.5
 find / -perm -2000 -ls 2>/dev/null
 
 echo -e "\n"
@@ -213,7 +242,7 @@ sleep 1
 
 # Listando Capabilities
 echo -e "\033[1;35m========================= 🛠️ Listando Capabilities 🛠️ =========================\033[0m"
-sleep 1
+sleep 0.5
 getcap -r / 2>/dev/null
 
 echo -e "\n"
@@ -221,160 +250,154 @@ sleep 1
 
 # Listando tareas cron
 echo -e "\033[1;36m========================= ⏰ Listando Tareas Cron ⏰ =========================\033[0m"
-sleep 1
-cat /etc/crontab
+sleep 0.5
+cat /etc/crontab 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando interfaces de red
 echo -e "\033[1;36m========================= 🌐 Listando Interfaces de Red 🌐 =========================\033[0m"
-sleep 1
-ip a
+sleep 0.5
+ifconfig 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando puertos abiertos del sistema
 echo -e "\033[1;34m========================= 🌐 Listando Puertos Abiertos del Sistema 🌐 =========================\033[0m"
-sleep 1
+sleep 0.5
 echo -e "\033[1;33m1: Listando Servicios TCP\033[0m"
-ss -tunlp | grep "tcp"
+sleep 0.5
+ss -tunlp | grep "tcp" 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m1: Listando Servicios UDP\033[0m"
-sleep 1
-ss -tunlp | grep "udp"
+sleep 0.5
+ss -tunlp | grep "udp" 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando el /proc/net
 echo -e "\033[1;36m========================= 🌐 Listando el Directorio /proc/net 🌐 =========================\033[0m"
-sleep 1
+sleep 0.5
 echo -e "\033[1;33m4: Listando Protocolo TCP.\033[0m"
-cat /proc/net/tcp
+sleep 0.5
+cat /proc/net/tcp 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m4: Listando Protocolo UDP.\033[0m"
-sleep 1
-cat /proc/net/udp
+sleep 0.5
+cat /proc/net/udp 2>/dev/null
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m5: Listando IPv6.\033[0m"
-sleep 1
-cat /proc/net/if_inet6
+sleep 0.5
+cat /proc/net/if_inet6 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando los archivos importantes de Apache
 echo -e "\033[1;32m========================= 🐘 Listando los Archivos Importantes de Apache 🐘 =========================\033[0m"
-sleep 1
+sleep 0.5
 echo -e "\033[1;33m1: Listando el Archivo de Configuración Principal de Apache.\033[0m"
-sleep 1
-cat /etc/apache2/apache2.conf | grep -ivE "#"
+sleep 0.5
+cat /etc/apache2/apache2.conf 2>/dev/null || true
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m2: Listando el Archivo de Configuración para un Sitio Virtual Predeterminado de Apache.\033[0m"
-sleep 1
-cat /etc/apache2/sites-available/000-default.conf | grep -ivE "#"
+sleep 0.5
+cat /etc/apache2/sites-available/000-default.conf 2>/dev/null || true
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m3: Listando el Archivo de Configuración para un Sitio Virtual con Soporte para HTTPS.\033[0m"
-sleep 1
-cat /etc/apache2/sites-available/default-ssl.conf | grep -ivE "#"
+sleep 0.5
+cat /etc/apache2/sites-available/default-ssl.conf 2>/dev/null || true
 
 echo -e "\n"
 sleep 1
 
 # Listando los archivos importantes de Nginx
 echo -e "\033[1;32m========================= 🌍 Listando los Archivos Importantes de Nginx 🌍 =========================\033[0m"
-sleep 1
-
+sleep 0.5
 echo -e "\033[1;33m1: Listando el Archivo de Configuración Principal de Nginx.\033[0m"
-sleep 1
-cat /etc/nginx/nginx.conf | grep -ivE "#"
+sleep 0.5
+cat /etc/nginx/nginx.conf 2>/dev/null || true
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m2: Listando el Archivo de Configuración para un Servidor Virtual Predeterminado de Nginx.\033[0m"
-sleep 1
-cat /etc/nginx/sites-available/default | grep -ivE "#"
+sleep 0.5
+cat /etc/nginx/sites-available/default 2>/dev/null || true
 
 echo -e "\n"
-sleep 1
 
 echo -e "\033[1;33m3: Listando el Archivo de Configuración para un Servidor Virtual con Soporte para HTTPS.\033[0m"
-sleep 1
-cat /etc/nginx/sites-available/default-ssl | grep -ivE "#"
+sleep 0.5
+cat /etc/nginx/sites-available/default-ssl 2>/dev/null || true
 
 echo -e "\n"
 sleep 1
 
 # Listando el directorio /opt
 echo -e "\033[1;34m========================= 📦 Listando el Directorio /opt 📦 =========================\033[0m"
-sleep 1
-ls -la /opt
+sleep 0.5
+ls -la /opt 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando directorio /tmp
 echo -e "\033[1;32m========================= 🗑️ Listando Archivos en /tmp 🗑️ =========================\033[0m"
-sleep 1
-ls -la /tmp
+sleep 0.5
+ls -la /tmp 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando el directorio /mnt
 echo -e "\033[1;33m========================= 🛠️ Listando el Directorio /mnt 🛠️ =========================\033[0m"
-sleep 1
-ls -la /mnt
+sleep 0.5
+ls -la /mnt 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando el directorio /var/backups
 echo -e "\033[1;31m========================= 💾 Listando el Directorio /var/backups 💾 =========================\033[0m"
-sleep 1
-ls -la /var/backups
+sleep 0.5
+ls -la /var/backups 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando el directorio /var/opt
 echo -e "\033[1;36m========================= 📂 Listando el Directorio /var/opt 📂 =========================\033[0m"
-sleep 1
-ls -la /var/opt
+sleep 0.5
+ls -la /var/opt 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando el directorio /var/mail
 echo -e "\033[1;35m========================= 📧 Listando el Directorio /var/mail 📧 =========================\033[0m"
-sleep 1
-ls -la /var/mail
+sleep 0.5
+ls -la /var/mail 2>/dev/null
 
 echo -e "\n"
 sleep 1
 
 # Listando el directorio /var/www
 echo -e "\033[1;32m========================= 🌐 Listando el Directorio /var/www 🌐 =========================\033[0m"
-sleep 1
-ls -la /var/www/
+sleep 0.5
+ls -la /var/www/ 2>/dev/null
 
 echo -e "\n"
 sleep 1
